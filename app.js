2b585968e01b350356016431b1fee6e6812779ca
Michael Bravo <michael.bravo777@gmail.com>
2019-06-10 16:51:50 -0400

    completed drills 1-3

--- app.js
+++ app.js
@@ -25,7 +25,51 @@ app.get('/sum', (req, res) => {
   res.send( sumNums.toString() );
 });
 
+app.get('/cipher', (req, res) => {
+  const text = req.query.text;
+  const shift = parseInt(req.query.shift);
+  let output = '';
+  for (let i=0;i < text.length; i++) {
+    if ( text.charCodeAt(i) === 90 || text.charCodeAt(i) === 122 ) {
+      output += String.fromCharCode(text.charCodeAt(i) + shift - 26);
+    } else {
+      output += String.fromCharCode(text.charCodeAt(i) + shift);
+    }
+  }
+  res.send( output.toString() );
+});
 
+app.get('/lotto', (req, res) => {
+  const numbers = req.query.arr;
+  let random = [];
+  let counter = 0;
+  let output = '';
+  for (let i=0;i < 6; i++){
+    random.push(Math.floor(Math.random() * 20));
+    for (let j=0;j < 6; j++){
+      if ( random[i] === parseInt(numbers[j]) ) {
+        counter = counter + 1;
+      }
+    }
+
+  }
+  if ( counter < 4 ) {
+    output = 'Sorry, you lose';
+  }
+  if ( counter === 4 ) {
+    output = 'Congratulations, you win a free ticket';
+  }
+  if ( counter === 5 ) {
+    output = 'Congratulations! You win $100!';
+  }
+
+  if ( counter === 6 ) {
+    output = 'Wow! Unbelievable! You could have won the mega millions!';
+  }
+
+  res.send( output.toString() );
+
+});
 
 app.listen(8000, () => {
   console.log('Express server is listening on port 8000!');
